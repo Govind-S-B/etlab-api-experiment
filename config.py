4c3889b9870ae81bae3f295c751233e9ac9e189b
Govind.S.B <b.s.dnivog@gmail.com>
2024-04-07 10:51:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     DEBUG = True
     USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-    BASE_URL = "https://rit.etlab.in"
-    COOKIE_KEY = "RITSESSIONID"
+    BASE_URL = "https://marian.etlab.in"
+    COOKIE_KEY = "MCESESSIONID"

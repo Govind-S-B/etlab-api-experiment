56bebf4b31a0ac3a2872ca1a29f8277616ca67a4
Govind S B <b.s.dnivog@gmail.com>
2024-04-07 14:49:00 +0530

    moved global variable

--- app/routes/login.py
+++ app/routes/login.py
@@ -7,7 +7,6 @@ from app.docs.swagger import swagger_login_spec
 from config import Config
 
 bp = Blueprint("login", __name__, url_prefix="/api")
-session = requests.Session()
 
 
 @bp.route("/login", methods=["POST"])
@@ -18,7 +17,7 @@ def login():
     password = body.get("password")
 
     if not username or not password:
-        return jsonify({"message": "Username and password is required"}), 401
+        return jsonify({"message": "Username and password are required"}), 401
 
     payload = {
         "LoginForm[username]": username,
@@ -31,13 +30,15 @@ def login():
         "Content-Type": "application/x-www-form-urlencoded",
     }
 
-    response = session.post(
-        f"{Config.BASE_URL}/user/login", data=payload, headers=headers
-    )
-    soup = BeautifulSoup(response.text, "html.parser")
-    title = soup.find("title").text
-    if "login" in title.lower():
-        return jsonify({"message": "Invalid username or password"}), 401
-    cookie = session.cookies.get_dict()[Config.COOKIE_KEY]
+    with requests.Session() as session:
+        response = session.post(
+            f"{Config.BASE_URL}/user/login", data=payload, headers=headers
+        )
+        soup = BeautifulSoup(response.text, "html.parser")
+        title = soup.find("title").text
+        if "login" in title.lower():
+            return jsonify({"message": "Invalid username or password"}), 401
+        cookie = session.cookies.get_dict()[Config.COOKIE_KEY]
 
     return jsonify({"message": "Login successful", "token": cookie}), 200
+
